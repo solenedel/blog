61043ab572c0dfe0f910e75ed3c7330a1cab94c9
solenedel <solene98@student.ubc.ca>
2021-12-01 19:36:24 -0800

    Updates

---
+++ static/js/main.746b6e43.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpblog=this.webpackJsonpblog||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),o=e.n(c),s=e(3),r=e.n(s),i=(e(8),e(9),e(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};r.a.render(Object(i.jsx)(o.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.746b6e43.chunk.js.map
