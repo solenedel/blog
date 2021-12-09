5628f0252a1264b98925395ee8e6b3a31309dd9f
solenedel <solene98@student.ubc.ca>
2021-12-09 10:28:25 -0800

    work on blog

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,20 @@
 import React, { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
   const [blogs, setBlogs] = useState([
     { title: 'my new blog', body: 'lorem ipsum', author: 'mario', id: 1 },
     { title: 'apple', body: 'banana', author: 'luigi', id: 2 },
-    { title: 'kiwi', body: 'pear', author: 'peach', id: 3 }
+    { title: 'kiwi', body: 'pear', author: 'mario', id: 3 }
   ]);
  
 
 
   return ( 
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-           <h2>{ blog.title }</h2>
-           <p>Written by { blog.author }</p>
-        </div>
-      ))}
-    
+      <BlogList blogs={blogs} title="All blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
     </div>
    );
 }
