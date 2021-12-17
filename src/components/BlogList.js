203849714e6aef69439075d4a2cc31d732e92004
solenedel <solene98@student.ubc.ca>
2021-12-17 12:30:56 -0800

    create fake database file

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
 
 
   return ( 
@@ -10,6 +10,7 @@ const BlogList = ({ blogs, title }) => {
         <div className="blog-preview" key={blog.id}>
            <h2>{ blog.title }</h2>
            <p>Written by { blog.author }</p>
+           <button onClick={() => handleDelete(blog.id)}>delete</button>
         </div>
       ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,13 +8,26 @@ const Home = () => {
     { title: 'apple', body: 'banana', author: 'luigi', id: 2 },
     { title: 'kiwi', body: 'pear', author: 'mario', id: 3 }
   ]);
+
+  const [name, setName] = useState('mario');
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  };
  
+
+  useEffect(() => {
+    console.log('useEffect ran');
+    console.log(name);
+  }, [name]);
 
 
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" />
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>change name</button>
+      <p>{name}</p>
     </div>
    );
 }
