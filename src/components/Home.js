6a81ba6c29876e81141992280ccdc520dbc6441a
solenedel <solene98@student.ubc.ca>
2021-12-17 14:16:55 -0800

    work on blog app

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,33 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
  
 
   useEffect(() => {
     fetch('http://localhost:8082/blogs') // get request
     .then(res => {
-      return res.json() // parse JSON into JS object
+      if (!res.ok) throw Error('could not fetch the data for that resource');
+
+      return res.json(); // parse JSON into JS object
     })
     .then(data => {
       console.log(data);
       setBlogs(data);
+      setIsLoading(false);
+      setError(null);
+    })
+    .catch((err) => {
+      setError(err.message);
+      setIsLoading(false);
     })
   }, []);
 
 
   return ( 
     <div className="home">
+      { error && <div>{error}</div> }
+      { isLoading && <div>loading...</div> }
       { blogs && <BlogList blogs={blogs} title="All blogs" /> }
     </div>
    );
