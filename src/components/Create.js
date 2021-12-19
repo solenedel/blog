b9ae968c692b58afc488347d98f95015cf107fd1
solenedel <solene98@student.ubc.ca>
2021-12-18 20:55:08 -0800

    redirection to home page after blog submission

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
@@ -6,6 +7,8 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
   const [isLoading, setIsLoading] = useState(false);
+
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,6 +25,7 @@ const Create = () => {
     .then(() => {
       console.log('new blog added');
       setIsLoading(false);
+      history.push('/');
     })
   };
 
